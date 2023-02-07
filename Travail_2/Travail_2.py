66148a2023bce883f7b32b4d439bff268adfcdff
pierrebriac <pierrebriacmetayer@gmail.com>
2023-02-07 12:59:03 -0500

    modif

---
+++ Travail_2/Travail_2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy
+import csv
+
+
+
+
+# Importer mon data frame
+
+# Charger le fichier CSV existant
+data = pd.read_csv("PierreBriacMétayerMariotti_données_psy3019-H23_20220211_distribution-human-rights-vdem.csv")
+data
